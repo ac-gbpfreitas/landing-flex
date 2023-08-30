16e6326446a197e6464b4c8bfb3c7b779a3cc513
Gustavo Freitas <ac.gbpfreitas@gmail.com>
2023-08-29 22:43:18 -0700

    MainComponent created and included. Waiting for the sub components

---
+++ src/components/MainContent.vue
@@ -0,0 +1,24 @@
+<template>
+    <main>
+        <!-- SERVICES SECTION -->
+        <ServicesComponent />
+        <!-- SERVICES SECTION END -->
+        <!-- PORTFOLIO SECTION -->
+        <!-- PORTFOLIO SECTION END -->
+        <!-- TEAM SECTION -->
+        <!-- TEAM SECTION END -->
+        <!-- CONTACT SECTION -->
+        <!-- CONTACT SECTION END -->
+    </main>
+</template>
+
+<script>
+import ServicesComponent from './maincontent/ServicesComponent.vue';
+
+export default {
+    name: "MainContent",
+    components : {
+
+    }
+}
+</script>
