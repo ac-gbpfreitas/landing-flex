d347f931cadb12ac181d39fce77c9686432749b5
Gustavo Freitas <ac.gbpfreitas@gmail.com>
2023-08-29 22:43:48 -0700

    Services Component Created

---
+++ src/components/maincontent/ServicesComponent.vue
@@ -0,0 +1,56 @@
+<template>
+    <section class="row services" id="services">
+        <article>
+            <h2>
+                our services
+            </h2>
+            <p>
+                Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+            </p>
+        </article>
+        <section>
+            <div>
+                <i class="fa-regular fa-file-code"></i>
+                <h4>
+                    HTML5 CODING
+                </h4>
+                <p>
+                    Lorem, ipsum dolor sit amet consectetur adipisicing elit. Asperiores aut natus fugiat optio quidem suscipit earum mollitia at recusandae blanditiis?
+                </p>
+            </div>
+            <div>
+                <i class="fa-solid fa-paper-plane"></i>
+                <h4>
+                    OUR SUPPORT
+                </h4>
+                <p>
+                    Lorem, ipsum dolor sit amet consectetur adipisicing elit. Asperiores aut natus fugiat optio quidem suscipit earum mollitia at recusandae blanditiis?
+                </p>
+            </div>
+            <div>
+                <i class="fa-solid fa-building-columns"></i>
+                <h4>
+                    OUR DESIGN
+                </h4>
+                <p>
+                    Lorem, ipsum dolor sit amet consectetur adipisicing elit. Asperiores aut natus fugiat optio quidem suscipit earum mollitia at recusandae blanditiis?
+                </p>
+            </div>
+            <div>
+                <i class="fa-solid fa-flask"></i>
+                <h4>
+                    GO FOR MOBILE
+                </h4>
+                <p>
+                    Lorem, ipsum dolor sit amet consectetur adipisicing elit. Asperiores aut natus fugiat optio quidem suscipit earum mollitia at recusandae blanditiis?
+                </p>
+            </div>
+        </section>
+    </section>
+</template>
+
+<script>
+export default {
+    name: "ServicesComponent"
+}
+</script>
